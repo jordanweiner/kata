1040a79767eaeeb74b3f02046625e31125efb064
Jordan Wiener <jordanwiener@Balbixs-MacBook-Pro.local>
2018-06-28 10:47:44 -0700

    Added in end of week 2 and week 3 code adjustments

--- add.js
+++ add.js
@@ -1,39 +1,34 @@
 function add(a) {
-    // empty string
-    if (a.length == 0) {
-        return 0;
+    var delimeter = ',';
+    if (a.includes("//")) {
+        delimeter = a.charAt(2);  // gets delimeter
     }
-    // string with 1 number
-    else if (!a.includes(",")) {
-        return parseInt(a, 10);
-    }
-    // string with 2 or more numbers
-    else {
-        var strCopy = a.substring(0);
-        var numStr;
-        var sum = 0;
 
-        while ( strCopy.includes(',') || strCopy.includes('\n') ) {
-            
-            numStr = strCopy.substring(0, strCopy.indexOf( findSeparator(strCopy) ));     
-            sum += parseInt(numStr, 10);                        // add value to sum
-            
-            /* Take number we just added to sum out of the string */
-            strCopy = strCopy.substring(strCopy.indexOf(findSeparator(strCopy)) + 1, strCopy.length);  
+    a = a.replace(/[\/]|\n/g, delimeter);    // replace all \n with ,
+    var nums = a.split(delimeter);      
+    var sum = 0;
+
+    var negativeArray = [];     // holds negative numbers
+    var i;
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != "") {
+            if (parseInt(nums[i], 10) < 0)
+                negativeArray.push(nums[i]);
+            else 
+                sum += parseInt(nums[i], 10);   // add int of string to sum
+        }
+    }
+
+    if (negativeArray.length != 0) {
+        var exceptionMessage = "negatives not allowed:";
+        for (i = 0; i < negativeArray.length; i++) {
+            exceptionMessage += (" " + negativeArray[i]);
         }
 
-        // add last number to sum
-        numStr = strCopy.substring(0, strCopy.length);
-        sum += parseInt(numStr, 10);
-        return sum;
+        throw exceptionMessage;
     }
-}
 
-function findSeparator(str) {
-    if (str.indexOf(',') < str.indexOf('\n') && str.includes(','))
-        return ',';
-    else if (str.indexOf(',') >= str.indexOf('\n') && str.includes('\n'))
-        return '\n';
+    return sum;
 }
 
 module.exports = add;
--- add.test.js
+++ add.test.js
@@ -40,11 +40,54 @@ describe('add suite week 2', function() {
     });
     
     test('test sum returned of unknown amount of nums', function() {
-        expect(add("4,4,4")).toBe(12);
+        expect(add("4,4,4,8")).toBe(20);
     });
 
     test('test sum returned with new lines between numbers', function() {
-        expect(add("4,4\n4")).toBe(12);
+        expect(add("4,4\n4,8")).toBe(20);
+    });
+
+    test('test sum returned with new lines between numbers', function() {
+        expect(add("3\n4\n4\n4")).toBe(15);
+    });
+
+    test('test sum returned with new lines between numbers', function() {
+        expect(add("4\n4")).toBe(8);
+    });
+
+    test('test sum returned with new lines between numbers', function() {
+        expect(add("4\n4\n3")).toBe(11);
+    });
+
+
+});
+
+
+describe('add suite week 3', function() {
+    test('test sum returned with different delimiters', function() {
+        expect(add("//;\n1;2")).toBe(3);
+    });
+
+    test('test sum returned with different delimeters', function() {
+        expect(add("///1/2/3\n4")).toBe(10);
+    });
+
+    test('call add with negative number throws exception', function() {
+        expect(() => {
+            add("//;\n-1;2")
+        }).toThrow('negatives not allowed: -1');
+    });
+
+    test('call add with negative number throws exception', function() {
+        expect(() => {
+            add("-10,90\n-45")
+        }).toThrow('negatives not allowed: -10 -45');
+    });
+
+    test('call add with negative number throws exception', function() {
+        expect(() => {
+            add("//;\n60;89\n-90\n-100")
+        }).toThrow('negatives not allowed: -90 -100');
     });
 
 });
